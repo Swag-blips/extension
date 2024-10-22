6af326975d1996a1888ea480ae9038183ddb0b8e
Swag-blips <coderblip@gmail.com>
2024-10-22 19:37:51 +0100

    feat:callback and token sent to backend

--- client/src/auth/Auth0providerWithNavigate.tsx
+++ client/src/auth/Auth0providerWithNavigate.tsx
@@ -1,21 +1,20 @@
 import { useCreateMyUser } from "@/api/UserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0providerWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const navigate = useNavigate();
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
+  const onRedirectCallback = () => {
+    navigate("/auth-callback");
   };
   if (!domain || !clientId || !redirectUri) {
     throw new Error("unable to initialize auth");
