f07331af315f4a903ba7caeb71296a88cc97735f
Swag-blips <coderblip@gmail.com>
2024-10-28 11:49:58 +0100

    feat:api-integration for updateUser

--- client/src/pages/UserProfilePage.tsx
+++ client/src/pages/UserProfilePage.tsx
@@ -1,7 +1,9 @@
+import { useUpdateMyUser } from "@/api/UserApi";
 import UserProfileForm from "@/forms/UserProfileForm/UserProfileForm";
 
 const UserProfilePage = () => {
-  return <UserProfileForm />;
+  const { updateUser, isLoading } = useUpdateMyUser();
+  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
 };
 
 export default UserProfilePage;
